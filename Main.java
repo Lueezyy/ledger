57bdf873adb43595a7f7566f6ae9e7b82cfbeb65
Lueezyy <luis.balb@icloud.com>
2026-09-25 14:23:26 -0500

    Print blank menu

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello, bank!");
+    public static void main(String[] args) {
+        System.out.println("Bank Application");
+        System.out.println("1. Create");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Show");
+        System.out.println("5. Quit");
+        System.out.print("Pick an option: ");
     }
 }
