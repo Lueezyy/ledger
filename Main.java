0eaaaef50cdad23260c5d65d3e6e929b2f458121
Lueezyy <luis.balb@icloud.com>
2026-09-25 14:29:35 -0500

    Loop menu until user picks quit

--- Main.java
+++ Main.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-        System.out.println("1. Create");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Show");
-        System.out.println("5. Quit");
-        System.out.print("Pick an option: ");
+        while (running) {
+            System.out.println("1. Create");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Show");
+            System.out.println("5. Quit");
+            System.out.print("Pick an option: ");
 
-        int choice = scanner.nextInt();
-        System.out.println("You picked " + choice);
+            int choice = scanner.nextInt();
+            System.out.println("You picked " + choice);
+
+            if (choice == 5) {
+                running = false;
+            }
+        }
 
         scanner.close();
     }
