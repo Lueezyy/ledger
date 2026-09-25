5aa671048c6720d7c31f6a1b7468995b882e7a95
Lueezyy <luis.balb@icloud.com>
2026-09-25 14:45:35 -0500

    Add accounts map

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        HashMap<String, Double> accounts = new HashMap<>();
         boolean running = true;
 
         while (running) {
@@ -14,7 +16,15 @@ public class Main {
             System.out.print("Pick an option: ");
 
             int choice = scanner.nextInt();
+            scanner.nextLine();
             System.out.println("You picked " + choice);
+
+            if (choice == 1) {
+                System.out.print("Account name: ");
+                String name = scanner.nextLine();
+                accounts.put(name, 0.0);
+                System.out.println("Account created.");
+            }
 
             if (choice == 5) {
                 System.out.println("Goodbye!");
