6a78e17ea7a6252c4aba74423c6076ece67453e8
Lueezyy <luis.balb@icloud.com>
2026-09-25 14:26:54 -0500

    Read menu choice

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bank Application");
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("1. Create");
         System.out.println("2. Deposit");
         System.out.println("3. Withdraw");
         System.out.println("4. Show");
         System.out.println("5. Quit");
         System.out.print("Pick an option: ");
+
+        int choice = scanner.nextInt();
+        System.out.println("You picked " + choice);
+
+        scanner.close();
     }
 }
